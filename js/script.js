71620e8fa83494ed444d84112ab93b50f006244a
Angel Hernandez <angelhernandez@Angels-MacBook-Pro.local>
2025-10-22 16:18:17 -0400

    Added toggle to the buttons to track the answers

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 console.log("script.js connected!");
+
+// Toggle button fill to track answers
+let blocks = document.querySelectorAll('.question-block');
+
+
+blocks.forEach((block) => {
+    let btns = block.querySelectorAll('.answer-btn');
+
+    btns.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            btns.forEach((b) => b.classList.remove('selected'));
+            btn.classList.add('selected');
+        })
+    })
+})
+
