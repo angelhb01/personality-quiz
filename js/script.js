b556606c2a81e8387948112c598a1fa4a9e4f910
Angel Hernandez <angelhernandez@Angels-MacBook-Pro.local>
2025-10-23 15:16:25 -0400

    stored the scores in a dictionary for each question

--- js/script.js
+++ js/script.js
@@ -3,6 +3,32 @@ console.log("script.js connected!");
 // Toggle button fill to track answers
 let blocks = document.querySelectorAll('.question-block');
 
+let scores = {
+    "question-1": 0,
+    "question-2": 0,
+    "question-3": 0,
+    "question-4": 0,
+    "question-5": 0
+}
+
+
+const results = (result, question) => {
+    switch(result) {
+        case "A":
+            scores[question] = 4;
+            break;
+        case "B":
+            scores[question] = 3;
+            break;
+        case "C":
+            scores[question] = 2;
+            break;
+        case "D":
+            scores[question] = 1;
+            break;
+    }
+    console.log(scores);
+}
 
 blocks.forEach((block) => {
     let btns = block.querySelectorAll('.answer-btn');
@@ -11,7 +37,7 @@ blocks.forEach((block) => {
         btn.addEventListener('click', () => {
             btns.forEach((b) => b.classList.remove('selected'));
             btn.classList.add('selected');
+            results(btn.getAttribute("data-answer"), block.parentNode.getAttribute('id'));
         })
     })
 })
-
